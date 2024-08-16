d7bb7d2e07ef76961147333bb1bcf69f14bb028b
Nilesh Shah <miral@addwebsolution.in>
2024-08-16 11:09:42 +0530

    Revert "Initial commit for sdc"
    This reverts commit 8aa500184f1172e3d22b468713208b29073a11f4.

--- web/modules/custom/tit/js/script.js
+++
@@ -1,8 +0,0 @@
-(function (Drupal) {
-  Drupal.behaviors.exampleBehavior = {
-    attach: function (context, settings) {
-      var message = Drupal.t('Hello, world! Sanket');
-      console.log(message);
-    }
-  };
-})(Drupal);
